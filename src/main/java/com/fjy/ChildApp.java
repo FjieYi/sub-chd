c7da52ce2ad3ef4fa30fa0bb5ef66ea4e75f2fdd
FjieYi <1197562890@qq.com>
2021-08-19 17:37:33 +0800

    init project

---
+++ src/main/java/com/fjy/ChildApp.java
@@ -0,0 +1,13 @@
+package com.fjy;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChildApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/fjy/ChildAppTest.java
@@ -0,0 +1,20 @@
+package com.fjy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ChildApp.
+ */
+public class ChildAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
